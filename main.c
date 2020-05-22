a92e7132d0a434b124da77414cf0c053c2d4ab29
mkonnerth <matkonnerth@gmail.com>
2020-05-22 11:37:06 +0200

    add redisgraphImport

--- main.c
+++ main.c
@@ -17,21 +17,19 @@ void iterate(UA_Server *server, const UA_NodeId *id) {
   UA_BrowseDescription_init(&bd);
   bd.browseDirection = UA_BROWSEDIRECTION_FORWARD;
   bd.includeSubtypes = true;
-  bd.referenceTypeId = UA_NODEID_NUMERIC(0, UA_NS0ID_HASSUBTYPE);
+  bd.referenceTypeId = UA_NODEID_NUMERIC(0, UA_NS0ID_HIERARCHICALREFERENCES);
   // bd.resultMask = UA_BROWSERESULTMASK_ALL;
   bd.resultMask = UA_BROWSERESULTMASK_BROWSENAME;
   bd.nodeId = *id;
-  bd.nodeClassMask = UA_NODECLASS_REFERENCETYPE;
+  bd.nodeClassMask = UA_NODECLASS_OBJECT;
   // bd.nodeClassMask
-  UA_BrowseResult br = UA_Server_browse(server, 100, &bd);
+  UA_BrowseResult br = UA_Server_browse(server, 1000, &bd);
   if (br.statusCode == UA_STATUSCODE_GOOD) {
     for (UA_ReferenceDescription *rd = br.references;
          rd != br.references + br.referencesSize; rd++) {
-      printf("found :printf %.*s\n", rd->browseName.name.length,
-             rd->browseName.name.data);
 
-      /*
-      const char* bn = "PublishedEventsType";
+      
+      const char* bn = "Heating";
       if (strlen(bn) == rd->browseName.name.length &&
           !strncmp(bn, rd->browseName.name.data,
                    strlen(bn)))
@@ -39,7 +37,6 @@ void iterate(UA_Server *server, const UA_NodeId *id) {
           printf("found :printf %.*s\n", rd->browseName.name.length,
                  rd->browseName.name.data);
       }
-      */
 
       iterate(server, &rd->nodeId.nodeId);
     }
@@ -58,12 +55,8 @@ int main(int argc, const char *argv[]) {
     }
   }
 
-  UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "start hierachicalrefs");
-  UA_NodeId startId = UA_NODEID_NUMERIC(0, UA_NS0ID_HIERARCHICALREFERENCES);
-  iterate(server, &startId);
-  UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
-              "start nonHierachicalRefs");
-  startId = UA_NODEID_NUMERIC(0, UA_NS0ID_NONHIERARCHICALREFERENCES);
+  UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "start");
+  UA_NodeId startId = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER);
   iterate(server, &startId);
   UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "finished");
 
